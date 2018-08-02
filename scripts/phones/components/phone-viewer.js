c05b80a8ad72c6459e34fa6f25d2cebbc7ed1428
erisdream <evgeniynedilko@mail.ru>
2018-08-02 16:04:35 +0300

    Add info in phone-viewer

--- scripts/phones/components/phone-viewer.js
+++ scripts/phones/components/phone-viewer.js
@@ -13,7 +13,7 @@ export default class PhoneViewer extends Component {
 
   _render() {
     this._element.innerHTML = `
-      <img class="phone" src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
+      <img class="phone" src="${ this._phone.images[0] }">
 
       <button>Back</button>
       <button>Add to basket</button>
@@ -21,28 +21,132 @@ export default class PhoneViewer extends Component {
   
       <h1>Motorola XOOM™ with Wi-Fi</h1>
   
-      <p>Motorola XOOM with Wi-Fi has a super-powerful dual-core processor and Android™ 3.0 (Honeycomb) — the Android platform designed specifically for tablets. With its 10.1-inch HD widescreen display, you’ll enjoy HD video in a thin, light, powerful and upgradeable tablet.</p>
+      <p>${ this._phone.description }</p>
   
       <ul class="phone-thumbs">
         <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
+          <img src="${ this._phone.images[0] }">
         </li>
         <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.1.jpg">
+          <img src="${ this._phone.images[1] }">
         </li>
         <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.2.jpg">
+          <img src="${ this._phone.images[2] }">
         </li>
         <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.3.jpg">
+          <img src="${ this._phone.images[3] }">
         </li>
         <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.4.jpg">
+          <img src="${ this._phone.images[4] }">
         </li>
         <li>
-          <img src="img/phones/motorola-xoom-with-wi-fi.5.jpg">
+          <img src="${ this._phone.images[5] }">
         </li>
       </ul>
+
+<ul class="specs">
+      <li>
+        <span>Availability and Networks</span>
+        <dl>
+          <dt>Availability</dt>
+          <dd>${ this._phone.availability })</dd>
+        </dl>
+      </li>
+      <li>
+        <span>Battery</span>
+        <dl>
+          <dt>Type</dt>
+          <dd>${ this._phone.battery.type })</dd>
+          <dt>Talk Time</dt>
+          <dd>${ this._phone.battery.talkTime }</dd>
+          <dt>Standby time (max)</dt>
+          <dd>${ this._phone.battery.standbyTime }</dd>
+        </dl>
+      </li>
+      <li>
+        <span>Storage and Memory</span>
+        <dl>
+          <dt>RAM</dt>
+          <dd>${ this._phone.storage.ram }</dd>
+          <dt>Internal Storage</dt>
+          <dd>${ this._phone.storage.flash }</dd>
+        </dl>
+      </li>
+      <li>
+        <span>Connectivity</span>
+        <dl>
+          <dt>Network Support</dt>
+          <dd></dd>
+          <dt>WiFi</dt>
+          <dd>${ this._phone.connectivity.wifi }</dd>
+          <dt>Bluetooth</dt>
+          <dd>${ this._phone.connectivity.bluetooth }</dd>
+          <dt>Infrared</dt>
+          <dd>${ this._phone.connectivity.infrared ? '✓' : '✘' }</dd>
+          <dt>GPS</dt>
+          <dd>${ this._phone.connectivity.gps ? '✓' : '✘' }</dd>
+        </dl>
+      </li>
+      <li>
+        <span>Android</span>
+        <dl>
+          <dt>OS Version</dt>
+          <dd>${ this._phone.android.os }</dd>
+          <dt>UI</dt>
+          <dd>${ this._phone.android.ui }</dd>
+        </dl>
+      </li>
+      <li>
+        <span>Size and Weight</span>
+        <dl>
+          <dt>Dimensions</dt>
+          <dd>${ this._phone.sizeAndWeight.dimensions[0] }</dd>
+          <dd>${ this._phone.sizeAndWeight.dimensions[1] }</dd>
+          <dd>${ this._phone.sizeAndWeight.dimensions[2] })</dd>
+          <dt>Weight</dt>
+          <dd>${ this._phone.sizeAndWeight.weight }</dd>
+        </dl>
+      </li>
+      <li>
+        <span>Display</span>
+        <dl>
+          <dt>Screen size</dt>
+          <dd>${ this._phone.display.screenSize }</dd>
+          <dt>Screen resolution</dt>
+          <dd>${ this._phone.display.screenResolution }</dd>
+          <dt>Touch screen</dt>
+          <dd>${ this._phone.display.touchScreen ? '✓' : '✘' }</dd>
+        </dl>
+      </li>
+      <li>
+        <span>Hardware</span>
+        <dl>
+          <dt>CPU</dt>
+          <dd>${ this._phone.hardware.cpu }</dd>
+          <dt>USB</dt>
+          <dd>${ this._phone.hardware.usb }</dd>
+          <dt>Audio / headphone jack</dt>
+          <dd>${ this._phone.hardware.audioJack }m</dd>
+          <dt>FM Radio</dt>
+          <dd>${ this._phone.hardware.fmRadio ? '✓' : '✘' }</dd>
+          <dt>Accelerometer</dt>
+          <dd>${ this._phone.hardware.accelerometer ? '✓' : '✘' }</dd>
+        </dl>
+      </li>
+      <li>
+        <span>Camera</span>
+        <dl>
+          <dt>Primary</dt>
+          <dd>${ this._phone.camera.primary }</dd>
+          <dt>Features</dt>
+          <dd>${ this._phone.camera.features }</dd>
+        </dl>
+      </li>
+      <li>
+        <span>Additional Features</span>
+        <dd>${ this._phone.additionalFeatures }</dd>
+      </li>
+    </ul>
     `;
   }
 }
--- scripts/phones/services/phone-service.js
+++ scripts/phones/services/phone-service.js
@@ -156,68 +156,69 @@ const phonesFromServer = [
   }
 ];
 const phoneFromServer = {
-  "additionalFeatures": "Front Facing 1.3MP Camera",
-  "android": {
-    "os": "Android 2.2",
-    "ui": "Dell Stage"
-  },
-  "availability": [
-    "T-Mobile"
-  ],
-  "battery": {
-    "standbyTime": "",
-    "talkTime": "",
-    "type": "Lithium Ion (Li-Ion) (2780 mAH)"
-  },
-  "camera": {
-    "features": [
-      "Flash",
-      "Video"
-    ],
-    "primary": "5.0 megapixels"
-  },
-  "connectivity": {
-    "bluetooth": "Bluetooth 2.1",
-    "cell": "T-mobile HSPA+ @ 2100/1900/AWS/850 MHz",
-    "gps": true,
-    "infrared": false,
-    "wifi": "802.11 b/g"
-  },
-  "description": "Introducing Dell\u2122 Streak 7. Share photos, videos and movies together. It\u2019s small enough to carry around, big enough to gather around. Android\u2122 2.2-based tablet with over-the-air upgrade capability for future OS releases.  A vibrant 7-inch, multitouch display with full Adobe\u00ae Flash 10.1 pre-installed.  Includes a 1.3 MP front-facing camera for face-to-face chats on popular services such as Qik or Skype.  16 GB of internal storage, plus Wi-Fi, Bluetooth and built-in GPS keeps you in touch with the world around you.  Connect on your terms. Save with 2-year contract or flexibility with prepaid pay-as-you-go plans",
-  "display": {
-    "screenResolution": "WVGA (800 x 480)",
-    "screenSize": "7.0 inches",
-    "touchScreen": true
-  },
-  "hardware": {
-    "accelerometer": true,
-    "audioJack": "3.5mm",
-    "cpu": "nVidia Tegra T20",
-    "fmRadio": false,
-    "physicalKeyboard": false,
-    "usb": "USB 2.0"
-  },
-  "id": "dell-streak-7",
-  "images": [
-    "img/phones/dell-streak-7.0.jpg",
-    "img/phones/dell-streak-7.1.jpg",
-    "img/phones/dell-streak-7.2.jpg",
-    "img/phones/dell-streak-7.3.jpg",
-    "img/phones/dell-streak-7.4.jpg"
-  ],
-  "name": "Dell Streak 7",
-  "sizeAndWeight": {
-    "dimensions": [
-      "199.9 mm (w)",
-      "119.8 mm (h)",
-      "12.4 mm (d)"
-    ],
-    "weight": "450.0 grams"
-  },
-  "storage": {
-    "flash": "16000MB",
-    "ram": "512MB"
-  }
+    "additionalFeatures": "Sensors: proximity, ambient light, barometer, gyroscope", 
+    "android": {
+        "os": "Android 3.0", 
+        "ui": "Honeycomb"
+    }, 
+    "availability": [
+        ""
+    ], 
+    "battery": {
+        "standbyTime": "336 hours", 
+        "talkTime": "24 hours", 
+        "type": "Other ( mAH)"
+    }, 
+    "camera": {
+        "features": [
+            "Flash", 
+            "Video"
+        ], 
+        "primary": "5.0 megapixels"
+    }, 
+    "connectivity": {
+        "bluetooth": "Bluetooth 2.1", 
+        "cell": "", 
+        "gps": true, 
+        "infrared": false, 
+        "wifi": "802.11 b/g/n"
+    }, 
+    "description": "Motorola XOOM with Wi-Fi has a super-powerful dual-core processor and Android\u2122 3.0 (Honeycomb) \u2014 the Android platform designed specifically for tablets. With its 10.1-inch HD widescreen display, you\u2019ll enjoy HD video in a thin, light, powerful and upgradeable tablet.", 
+    "display": {
+        "screenResolution": "WXGA (1200 x 800)", 
+        "screenSize": "10.1 inches", 
+        "touchScreen": true
+    }, 
+    "hardware": {
+        "accelerometer": true, 
+        "audioJack": "3.5mm", 
+        "cpu": "1 GHz Dual Core Tegra 2", 
+        "fmRadio": false, 
+        "physicalKeyboard": false, 
+        "usb": "USB 2.0"
+    }, 
+    "id": "motorola-xoom-with-wi-fi", 
+    "images": [
+        "img/phones/motorola-xoom-with-wi-fi.0.jpg", 
+        "img/phones/motorola-xoom-with-wi-fi.1.jpg", 
+        "img/phones/motorola-xoom-with-wi-fi.2.jpg", 
+        "img/phones/motorola-xoom-with-wi-fi.3.jpg", 
+        "img/phones/motorola-xoom-with-wi-fi.4.jpg", 
+        "img/phones/motorola-xoom-with-wi-fi.5.jpg"
+    ], 
+    "name": "Motorola XOOM\u2122 with Wi-Fi", 
+    "sizeAndWeight": {
+        "dimensions": [
+            "249.1 mm (w)", 
+            "167.8 mm (h)", 
+            "12.9 mm (d)"
+        ], 
+        "weight": "708.0 grams"
+    }, 
+    "storage": {
+        "flash": "32000MB", 
+        "ram": "1000MB"
+    }
 };
 
 
